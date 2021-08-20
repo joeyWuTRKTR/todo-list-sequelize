c40f39e2ab3d871f72d40e3fee96fd191e445b64
joeyWuTRKTR <godatascial@yahoo.com>
2021-08-20 14:52:34 +0800

    feat: add isAuthenticated to display view templates

--- app.js
+++ app.js
@@ -22,8 +22,13 @@ app.use(session({
 
 usePassport(app)
 
-app.use(routes)
+app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.isAuthenticated()
+  res.locals.user = req.user
+  next()
+})
 
+app.use(routes)
 
 app.listen(PORT, () => {
   console.log(`App is running on http://localhost:${PORT}`)
