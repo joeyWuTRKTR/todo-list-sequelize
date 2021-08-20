feec71d3144d79a82f1f07428adc8be57c407043
joeyWuTRKTR <godatascial@yahoo.com>
2021-08-20 15:39:11 +0800

    feat: add relations between todos and home router

--- routes/modules/todos.js
+++ routes/modules/todos.js
@@ -6,8 +6,11 @@ const Todo = db.Todo
 
 // Read
 router.get('/:id', (req, res) => {
+  const UserId = req.user.id
   const id = req.params.id
-  return Todo.findByPk(id)
+  return Todo.findOne({ 
+    where: { UserId, id }
+  })
     .then(todo => res.render('detail', { todo: todo.toJSON() }))
     .catch(error => console.log(error))
 })
