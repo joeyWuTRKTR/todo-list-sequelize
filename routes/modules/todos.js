bc53260d7ccc06ca21086490d787dc4651472d81
joeyWuTRKTR <godatascial@yahoo.com>
2021-08-20 15:41:37 +0800

    feat: add create router

--- routes/modules/todos.js
+++ routes/modules/todos.js
@@ -4,6 +4,20 @@ const router = express.Router()
 const db = require('../../models')
 const Todo = db.Todo
 
+// Create
+router.get('/new', (req, res) => {
+  return res.render('new')
+})
+
+router.post('/', (req, res) => {
+  const UserId = req.user.id
+  const name = req.body.name
+
+  return Todo.create({ name, UserId })
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
+})
+
 // Read
 router.get('/:id', (req, res) => {
   const UserId = req.user.id
